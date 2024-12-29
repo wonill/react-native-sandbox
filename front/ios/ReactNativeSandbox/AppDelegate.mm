20b76a407b5e187324d22c964869e703f7c99947
wonil <nwi6959@gmail.com>
2024-12-29 23:15:10 +0900

    ios 위치 권한 설정

--- front/ios/ReactNativeSandbox/AppDelegate.mm
+++ front/ios/ReactNativeSandbox/AppDelegate.mm
@@ -1,11 +1,13 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyBA6YrdySgLbvA-Krqlgcy94H7KPAr6qc0"]; // add this line using the api key obtained from Google Console
   self.moduleName = @"ReactNativeSandbox";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
